56910a5ef826344b6fdf64ded5530423c1d4d9d7
mercury0517 <aki19960517@gmail.com>
2023-04-02 17:28:21 +0900

    プロジェクトの作成

---
+++ sampleMVVM/sampleMVVM/sampleMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleMVVMApp.swift
+//  sampleMVVM
+//
+//  Created by 伊原明宏 on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct sampleMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
